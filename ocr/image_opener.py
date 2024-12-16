d24a6d929e03b7255c8e2c33644a1f1cf95585a7
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-16 17:20:01 +0300

    feat: add easyocr simple recognition

--- ocr/image_opener.py
+++ ocr/image_opener.py
@@ -1,9 +1,13 @@
 import cv2 as cv
+import easyocr
 
 def view_image(image):
     cv.imshow("image", image)
     cv.waitKey(0)
     cv.destroyAllWindows()
+
+
+reader = easyocr.Reader(['en'])
 
 image = cv.imread("ocr/sample.png")
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -15,5 +19,9 @@ _, bw = cv.threshold(gray, 200, 255, cv.THRESH_BINARY)
 clahe = cv.createCLAHE(clipLimit=10.0, tileGridSize=(10, 10))
 bw = clahe.apply(bw)
 
-view_image(image)
-view_image(bw)
+# view_image(image)
+# view_image(bw)
+
+results = reader.readtext(bw)
+for bbox, text, prob in results:
+    print(f"Bounding Box: {bbox}, Text: {text}, Probability: {prob}")
