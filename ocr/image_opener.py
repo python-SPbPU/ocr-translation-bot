0fe0683a74ce869770e34af660d71f970b607e2c
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-16 18:06:53 +0300

    feat: enhance image processing and text extraction in ocr

--- ocr/image_opener.py
+++ ocr/image_opener.py
@@ -7,21 +7,42 @@ def view_image(image):
     cv.destroyAllWindows()
 
 
+def is_on_same_line(y1, y2, threshold=10):
+    return abs(y1 - y2) < threshold
+
+
 reader = easyocr.Reader(['en'])
 
 image = cv.imread("ocr/sample.png")
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 # Бинаризация (преобразование в черно-белое)
-_, bw = cv.threshold(gray, 200, 255, cv.THRESH_BINARY)
+_, bw = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
 
 # Улучшение контраста
-clahe = cv.createCLAHE(clipLimit=10.0, tileGridSize=(10, 10))
+clahe = cv.createCLAHE(clipLimit=30.0, tileGridSize=(8, 8))
 bw = clahe.apply(bw)
+
+cv.imwrite("bw.png", bw)
 
 # view_image(image)
 # view_image(bw)
 
 results = reader.readtext(bw)
+strings = []
+i = 0
 for bbox, text, prob in results:
-    print(f"Bounding Box: {bbox}, Text: {text}, Probability: {prob}")
+    tl, tr, br, bl = bbox
+    # print(tl, text)
+    if len(strings) == 0:
+        strings.append((tl, text))
+        i += 1
+    else:
+        if is_on_same_line(tl[1], strings[i - 1][0][1]):
+            strings[i - 1] = (strings[i - 1][0], strings[i - 1][1] + " " + text)
+        else:
+            strings.append((tl, text))
+            i += 1
+
+for string in strings:
+    print(string[1])
