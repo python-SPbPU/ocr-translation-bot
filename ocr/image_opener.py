b5888de788a7c19e63d403db7393d54308124adf
StanleyStanMarsh <iastafev56@gmail.com>
2024-12-16 18:20:09 +0300

    refactor: encapsulate image processing in ImageOpener class

--- ocr/image_opener.py
+++ ocr/image_opener.py
@@ -11,19 +11,38 @@ def is_on_same_line(y1, y2, threshold=10):
     return abs(y1 - y2) < threshold
 
 
+class ImageOpener:
+    def __init__(self, image_path):
+        self.image_path = image_path
+        self.img = self.__open_image()
+
+    def __open_image(self):
+        return cv.imread(self.image_path)
+
+    def __binarize_image(self, img, low_threshold=150, high_threshold=255):
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        _, bw = cv.threshold(gray, low_threshold, high_threshold, cv.THRESH_BINARY)
+        return bw
+
+    def __enhance_contrast(self, img, clip_limit=30.0, tile_grid_size=(8, 8)):
+        clahe = cv.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
+        bw = clahe.apply(img)
+        return bw
+
+    def process_image(self, low_threshold=150, high_threshold=255, clip_limit=30.0, tile_grid_size=(8, 8)):
+        bw = self.__binarize_image(self.img, low_threshold, high_threshold)
+        bw = self.__enhance_contrast(bw, clip_limit, tile_grid_size)
+        return bw
+    
+    def write_image(self, img, path):
+        cv.imwrite(path, img)
+
+
 reader = easyocr.Reader(['en'])
 
-image = cv.imread("ocr/sample.png")
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
-# Бинаризация (преобразование в черно-белое)
-_, bw = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
-
-# Улучшение контраста
-clahe = cv.createCLAHE(clipLimit=30.0, tileGridSize=(8, 8))
-bw = clahe.apply(bw)
-
-cv.imwrite("bw.png", bw)
+im_o = ImageOpener("ocr/sample.png")
+bw = im_o.process_image(low_threshold=150, high_threshold=255, clip_limit=30.0, tile_grid_size=(8, 8))
+im_o.write_image(bw, "bw.png")
 
 # view_image(image)
 # view_image(bw)
