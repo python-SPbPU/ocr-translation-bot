b30c6e194d9b226716d712f7d1d0a83d57212f69
Anastasiya <Lozhkina_Anastasya@list.ru>
2025-01-14 08:40:34 +0300

    add main for basic func

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,27 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 from dotenv import load_dotenv
 from commands import router as commands_router
-
 import os
+
+
 load_dotenv()
 
+async def setup_bot_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Запустить бота"),
+        BotCommand(command="help", description="Получить помощь"),
+    ]
+    await bot.set_my_commands(commands)
+
 async def main():
-    bot = Bot(os.getenv('TOKEN'))
+    bot = Bot(os.getenv('TOKEN'), html=True)
     dp = Dispatcher()
 
     dp.include_router(commands_router)
 
+    await setup_bot_commands(bot)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
